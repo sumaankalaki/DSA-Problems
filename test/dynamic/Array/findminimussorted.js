84421fa73ca40f6c73a87278250fe6ec70649eec
Suma <suma.ankalaki@gmail.com>
2024-11-04 14:48:41 +0530

    GrapgQL 1- hour Completed

--- test/dynamic/Array/findminimussorted.js
+++ test/dynamic/Array/findminimussorted.js
@@ -10,25 +10,27 @@ Input: nums = [4,5,6,7,0,1,2]
 Output: 0
 Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
 */
+
 var findMin = function(nums) {
- let l=0; r=nums.length;
- let ans = Infinity;
-  while( l<r)
-  {
-    if (nums[l] <= nums[r]) {
-        ans = Math.min(ans, nums[l]);
-        break;
+    let l = 0, r = nums.length - 1;
+    let ans = nums[0];
+
+    while (l <= r) {
+        if (nums[l] <= nums[r]) {
+            ans = Math.min(ans, nums[l]);
+            break; // This can be added to improve efficiency when the array is already sorted
+        }
+
+        let mid = Math.floor((l + r) / 2);
+
+        ans = Math.min(ans, nums[mid]);
+
+        if (nums[l] <= nums[mid]) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
     }
 
-    let mid = math.floor((l+r)/2);
-    if(nums[l]<nums[mid])
-    {
-        res= Math.min(res, nums[l]);
-        low= mid+1;
-    }else{
-        res = Math.min(ans, nums[mid]);
-        high = mid - 1;
-    }
-  }
-  return ans;
-}
+    return ans;
+};
