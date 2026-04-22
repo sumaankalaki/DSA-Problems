fe4f87e32fefb95d1f108173ca21bfeb023fbe45
sumaankalaki <53658652+sumaankalaki@users.noreply.github.com>
2026-04-22 13:46:59 +0000

    distancedistanceRecursive

---
+++ 2026/dynamic/Edit Distance.js
@@ -0,0 +1,33 @@
+//Given 2 string word1 and word2 retur the maximun number of operation required to convert word1 and word2
+
+//Recursion Approach
+var distanceRecursive = function(word1,  word2){
+    let n = word1.length;
+    let m =  word2.length;
+
+    var number_of_operation = function(index1, index2){
+        //base case
+        if(index1>n-1 && index2>m-1){
+            return 0;
+        }
+        if(index1 >n-1){
+            return m-index2
+        }
+        if(index2 >m-1){
+            return n-index1;
+        }
+
+        //RECURSIVE
+        if(word1[index1]===word2[index2]){
+           return number_of_operation(index1 + 1, index2 + 1);
+        }
+        var insert = 1 + number_of_operation(index1, index2 + 1);
+        var delet = 1 + number_of_operation(index1 + 1, index2);
+        var replace = 1 + number_of_operation(index1 + 1, index2 + 1);
+        return Math.min(insert, delet, replace);
+    }
+    return number_of_operation(0,0);
+}
+
+console.log(distanceRecursive("abcedef", "acef"));
+console.log(distanceRecursive("abc", "adc"));
