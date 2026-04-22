e561d057ad21e5bf086e39fb4882d879bf805cf7
sumaankalaki <53658652+sumaankalaki@users.noreply.github.com>
2026-04-22 13:52:56 +0000

    distanceTabulation

--- 2026/dynamic/Edit Distance.js
+++ 2026/dynamic/Edit Distance.js
@@ -70,3 +70,34 @@ var distanceMemoization = function(word1,  word2){
 
 console.log(distanceMemoization("abcedef", "acef"));
 console.log(distanceMemoization("abc", "adc"));
+
+// Tabulation Approach/ bottom up approach
+var distanceTabulation = function(word1,  word2){
+    let n = word1.length;
+    let m =  word2.length;
+    let dp = new Array(n+1).fill(-1).map(()=>new Array(m+1).fill(-1));
+
+    for(let i=0; i<=n; i++){
+        dp[i][0] = i;
+    }
+    for(let j=0; j<=m; j++){
+        dp[0][j] = j;
+    }
+
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<=m; j++){
+            if(word1[i-1]===word2[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            }else{
+                var insert = 1 + dp[i][j-1];
+                var delet = 1 + dp[i-1][j];
+                var replace = 1 + dp[i-1][j-1];
+                dp[i][j] = Math.min(insert, delet, replace);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+console.log(distanceTabulation("abcedef", "acef"));
+console.log(distanceTabulation("abc", "adc"));
