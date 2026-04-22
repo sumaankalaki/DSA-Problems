27431000b79cb173cf4c0d2147362b9db57bb7ea
sumaankalaki <53658652+sumaankalaki@users.noreply.github.com>
2026-04-22 13:49:39 +0000

    distanceMemoization

--- 2026/dynamic/Edit Distance.js
+++ 2026/dynamic/Edit Distance.js
@@ -31,3 +31,42 @@ var distanceRecursive = function(word1,  word2){
 
 console.log(distanceRecursive("abcedef", "acef"));
 console.log(distanceRecursive("abc", "adc"));
+
+
+// Memoization Approach
+var distanceMemoization = function(word1,  word2){
+    let n = word1.length;
+    let m =  word2.length;
+    let memo = new Array(n).fill(-1).map(()=>new Array(m).fill(-1));
+
+    var number_of_operation = function(index1, index2){
+        //base case
+        if(index1>n-1 && index2>m-1){
+            return 0;
+        }
+        if(index1 >n-1){
+            return m-index2
+        }
+        if(index2 >m-1){
+            return n-index1;
+        }
+
+        if(memo[index1][index2]!==-1){
+            return memo[index1][index2];
+        }
+
+        //RECURSIVE
+        if(word1[index1]===word2[index2]){
+           return number_of_operation(index1 + 1, index2 + 1);
+        }
+        var insert = 1 + number_of_operation(index1, index2 + 1);
+        var delet = 1 + number_of_operation(index1 + 1, index2);
+        var replace = 1 + number_of_operation(index1 + 1, index2 + 1);
+        memo[index1][index2] = Math.min(insert, delet, replace);
+        return memo[index1][index2];
+    }
+    return number_of_operation(0,0);
+}
+
+console.log(distanceMemoization("abcedef", "acef"));
+console.log(distanceMemoization("abc", "adc"));
