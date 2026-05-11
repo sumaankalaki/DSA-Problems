36d64bfdb2b5630dd9b87538c97ee8151e580733
sumaankalaki <53658652+sumaankalaki@users.noreply.github.com>
2026-05-11 14:29:23 +0000

    binarySearchRecursive

--- 2026/Searching Algorithm/binarysearchTarget.js
+++ 2026/Searching Algorithm/binarysearchTarget.js
@@ -14,3 +14,24 @@ const binarySearchTarget = function(nums, target){
 
     return -1;
 }
+
+//Recursion
+
+const binarySearchRecursive = function(nums, target){
+
+    function helper(nums,target, left, right){
+        if(left>right) return -1;
+       const  mid = Math.floor((left+right)/2);
+        if(target===nums[mid]){
+             return mid;
+        } else if(target < nums[mid]){
+           helper(nums,target, left, mid-1);
+        } else{
+            helper(nums,target, mid+1, right);
+        }
+    }
+
+    return helper(nums,target, 0, nums.length-1);
+}
+
+console.log(binarySearchRecursive([1, 2, 3, 4, 5], 4));
