1d84ccbe33b5687ead4fc6d9169768b58dfb7033
sumaankalaki <53658652+sumaankalaki@users.noreply.github.com>
2026-05-15 01:29:36 +0530

    Update KthGrammer.js

--- 2026/Recursion+BackTraacking/KthGrammer.js
+++ 2026/Recursion+BackTraacking/KthGrammer.js
@@ -1,17 +1,16 @@
 //https://leetcode.com/problems/k-th-symbol-in-grammar/description/
 
 
-var  KthGrammer = function(n, k){
-    
+var KthGrammer = function(n, k){
     if(n===1){
         return 0;
     }
-    let len = Math.pow(2,n-1);
-    mid= len/2;
-    if(k<=mid){
-        return KthGrammer(n-1,k);
-    }else{
-        return 1-KthGrammer(n-1,k-1);
+    let len = Math.pow(2, n-1);
+    let mid = len/2;  // 👈 add let
+    if(k <= mid){
+        return KthGrammer(n-1, k);
+    } else {
+        return 1 - KthGrammer(n-1, k-mid);  // 👈 k-mid not k-1
     }
 }
-console.log(KthGrammer(4,8));
+console.log(KthGrammer(4, 8));
